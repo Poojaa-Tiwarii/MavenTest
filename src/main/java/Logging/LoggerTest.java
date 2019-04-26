c16690e768a7ffcb577b17ba99d7609bf86af801
Sumanth Muppasani <sumanthmuppasani@Sumanths-MacBook-Pro.local>
2019-04-25 20:25:22 -0700

    added comments

--- src/main/java/Logging/LoggerTest.java
+++ src/main/java/Logging/LoggerTest.java
@@ -1,3 +1,4 @@
+//Coomiit
 package Logging;
 
 import org.apache.log4j.Logger;
@@ -6,14 +7,14 @@ import org.apache.log4j.PropertyConfigurator;
 public class LoggerTest {
 
 	static String configFileName = "C:\\Users\\Pooja\\eclipse-workspace\\1.1\\log4j.properties";
-	
+
 	public static final Logger logger = Logger.getLogger(LoggerTest.class.getName());
 	//logger is class name i ma gettign from logger lib
 	public static void getLogger() {
-	
+
 		PropertyConfigurator.configure(configFileName);
 	}
-	
+
 
 	public static void testLogger() {
 		getLogger();
@@ -23,7 +24,7 @@ public class LoggerTest {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		testLogger();
 
 	}
--- src/main/java/testNg/BasicAnnotationsInTestNG.java
+++ src/main/java/testNg/BasicAnnotationsInTestNG.java
@@ -1,3 +1,4 @@
+//Comment
 package testNg;
 
 import org.testng.annotations.AfterClass;
@@ -14,38 +15,38 @@ public class BasicAnnotationsInTestNG {
 	public void test1() {
 		System.out.println("you are in your before test");
 	}
-	
+
 	@BeforeClass
 	public void test2() {
 		System.out.println("you are in your before class");
 	}
-	
+
 	@BeforeMethod
 	public void test3() {
 		System.out.println("you are in your befor emethod");
 	}
-	
+
 	@Test
 	public void test4() {
 		System.out.println("you are in your 1st test metho");
 	}
-	
+
 	@Test
 	public void test5() {
 		System.out.println("you are in your 2nd test metho");
 	}
-	
+
 
 	@AfterTest
 	public void test6() {
 		System.out.println("you are in your after test");
 	}
-	
+
 	@AfterClass
 	public void test7() {
 		System.out.println("you are in your  after class");
 	}
-	
+
 	@AfterMethod
 	public void test8() {
 		System.out.println("you are in your  after method");
